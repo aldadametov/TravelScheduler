a4cbded8b9e9d957e1276dfd12c11f8e68b12a29
Alisher Dadametov <ksindao@gmail.com>
2024-03-18 00:12:17 +0300

    initial commit

---
+++ TravelScheduler/TravelScheduler/TravelSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelSchedulerApp.swift
+//  TravelScheduler
+//
+//  Created by Алишер Дадаметов on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
