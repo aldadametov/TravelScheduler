3ba6e5c0c5a6890c34fa7b88d2218ddd2a6fe1c5
Alisher Dadametov <ksindao@gmail.com>
2024-05-17 23:37:08 +0300

    Added NoInternetView and ServerErrorView

---
+++ TravelScheduler/TravelScheduler/Views/NoInternetView.swift
@@ -0,0 +1,32 @@
+//
+//  NoInternetView.swift
+//  TravelScheduler
+//
+//  Created by Алишер Дадаметов on 17.05.2024.
+//
+
+import SwiftUI
+
+struct NoInternetView: View {
+    var body: some View {
+        VStack {
+            Image("no_internet_image")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 223, height: 223)
+            
+            Text("Нет интернета")
+                .font(.system(size: 24, weight: .bold))
+                .foregroundColor(.ypBlack)
+        }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: CustomBackButton())
+    }
+}
+
+struct NoInternetView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoInternetView()
+    }
+}
+
